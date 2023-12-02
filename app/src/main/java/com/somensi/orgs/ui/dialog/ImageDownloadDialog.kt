2c3e9ccbe766922c4b98615a2c15620f751661ee
Pedro Victor Somensi <psomensi.contato@gmail.com>
2023-12-02 20:59:38 -0300

    improved ImageDownloadDialog

--- app/src/main/java/com/somensi/orgs/ui/dialog/ImageDownloadDialog.kt
+++ app/src/main/java/com/somensi/orgs/ui/dialog/ImageDownloadDialog.kt
@@ -9,27 +9,32 @@ import com.somensi.orgs.utils.getText
 
 class ImageDownloadDialog(private val context: Context) {
 
-    fun show(urlDefault: String? = null, whenImageLoaded: (String?) -> Unit) {
-        val binding = FormImageBinding.inflate(LayoutInflater.from(context))
+    fun show(urlDefault: String? = null,
+             whenImageLoaded: (String?) -> Unit
+    ) {
 
-        urlDefault?.let {
-            binding.formImageImageview.downloadImage(it)
-            binding.formImageUrl.setText(it)
-        }
+        FormImageBinding.inflate(LayoutInflater.from(context)).apply {
 
-        binding.formImageButtonLoad.setOnClickListener {
-            val url = binding.formImageUrl.getText
-            binding.formImageImageview.downloadImage(url)
-        }
-
-        AlertDialog.Builder(context)
-            .setView(binding.root)
-            .setPositiveButton("Confirmar") { _, _ ->
-                val url = binding.formImageUrl.getText
-                whenImageLoaded(url)
+            urlDefault?.let {
+                formImageImageview.downloadImage(it)
+                formImageUrl.setText(it)
             }
-            .setNegativeButton("Cancelar") { _, _ -> }
-            .show()
+
+            formImageButtonLoad.setOnClickListener {
+                val url = formImageUrl.getText
+                formImageImageview.downloadImage(url)
+            }
+
+            AlertDialog.Builder(context)
+                .setView(root)
+                .setPositiveButton("Confirmar") { _, _ ->
+                    val url = formImageUrl.getText
+                    whenImageLoaded(url)
+                }
+                .setNegativeButton("Cancelar") { _, _ -> }
+                .show()
+
+        }
 
     }
 
